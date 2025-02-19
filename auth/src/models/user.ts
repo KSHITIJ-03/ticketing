c8a06ab1e66129ca30d44ec9d7e0cb80aff76a64
kshitij-03 <yadavkshitij392001@gmail.com>
2025-02-20 02:30:16 +0530

    add: statics method to user model, iterfaces for UserDoc

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,6 +5,19 @@ interface UserAttr {
     password : string
 }
 
+// interface that escribes the properties that a user model has
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attr : UserAttr) : UserDoc
+}
+
+// interface that describes the user document
+
+interface UserDoc {
+    email : string,
+    password : string
+}
+
 const userSchema = new mongoose.Schema({
     email : {
         type : String,
@@ -16,7 +29,24 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attr : UserAttr): UserDoc => {
+    return new User(attr);
+}
+
+
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+// User.build({
+//     email : 'tim@gmail.com',
+//     password : '12345678'
+// }) // typeScript don't know how to statics methods on model
+             // therefore it is needed to make a interface and do all that angular bracket stuff
+
+const user = User.build({
+    email : 'tim@gmail.com',
+    password : '12345678'
+}) 
 
 // new User({
 //     email : 'tim@gmail.com',
@@ -26,19 +56,19 @@ const User = mongoose.model('User', userSchema);
 //                    // will be of type interfave UserModel
 // })
 
-const buildUser = (attr: UserAttr) => {
-    return new User(attr);
-}
+// const buildUser = (attr: UserAttr) => {
+//     return new User(attr);
+// }
 
-buildUser({
-    email : 'tim@gmail.com',
-    password : '12345678',
-    //hello : 'jkdfsnc'          // this would be checked by typescript
-})
+// buildUser({
+//     email : 'tim@gmail.com',
+//     password : '12345678',
+//     //hello : 'jkdfsnc'          // this would be checked by typescript
+// })
 
 // to make new user attributes to be checked by TypeScript we will make a interface for User
 
-export {User, buildUser}; // but here it is not ideal to export buildUser function and import it
+export {User}; // but here it is not ideal to export buildUser function and import it
                           // every time for creating a user
 
                           // a alternative way is to attach that build function on the model
